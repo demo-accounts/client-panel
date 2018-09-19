df2eb00738b82d2550710fb489b9da0e7362c6aa
sidou01 <sidou@debian>
2018-09-19 17:03:00 +0100

    H

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import AppNavbar from "./Components/layout/AppNavbar";
 import Dashboard from "./Components/layout/Dashboard";
 import AddClient from './Components/clients/AddClient'
-import { hashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 class App extends Component {
   render() {
     return (
